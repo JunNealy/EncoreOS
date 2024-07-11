55337b810af5a5c80b06b9dbbe270867904bab82
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-07-11 16:38:47 -0400

    taskbar now displays corresponding buttons for the open apps and will bring them to front on click

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -1,7 +1,7 @@
 import encoreLogo from '../../assets/copyENCORELOGOCANDIDATEpx10.png';
 import Button from '../Button/Button';
 import SystemTray from '../SystemTray/SystemTray';
-
+import { applicationIndex } from '../../configs/applicationIdnex';
 import './Toolbar.scss';
 
 const Toolbar = ({
@@ -26,16 +26,27 @@ const Toolbar = ({
           onClick={handleClick}
           style={!displayStartMenu ? 'button' : 'button-depressed'}
         />
-        {openApplications.map((application, index) => (
-          <Button
-            key={index}
-            index={index}
-            icon={'test'}
-            label={'test'}
-            style={'button'}
-            onClick={bringToFront}
-          />
-        ))}
+        {openApplications.map((application, index) => {
+          const appInfo = applicationIndex.applications.find(
+            (app) => app.app === application.name
+          );
+          if (!appInfo) {
+            console.error(
+              `No matching application found for displayName: ${application.name}`
+            );
+            return null;
+          }
+          return (
+            <Button
+              key={index}
+              index={index}
+              icon={appInfo.icon}
+              label={appInfo.name}
+              style={'button'}
+              onClick={() => bringToFront(application.id)}
+            />
+          );
+        })}
       </div>
       <SystemTray />
     </div>
