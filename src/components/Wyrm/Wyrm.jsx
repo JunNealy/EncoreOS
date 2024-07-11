37306628de85e15497ada5d6a1718a32660f513e
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-07-11 17:48:44 -0400

    Started rendering board and snake with 2d array through iteration, need to add state for game element management

--- src/components/Wyrm/Wyrm.jsx
+++ src/components/Wyrm/Wyrm.jsx
@@ -1,5 +1,18 @@
+const BOARD_SIZE = 15;
+const STARTING_WYRM = [{ x: 2, y: 2 }];
+
 const Wyrm = () => {
-  return <div>Snake</div>;
+  const createBoard = () => {
+    const board = [];
+    for (let row = 0; row < BOARD_SIZE; row++) {
+      const columns = [];
+      for (let column = 0; column < BOARD_SIZE; column++) {
+        const isWyrm = wyrm;
+      }
+    }
+  };
+
+  return <div className="wyrm"></div>;
 };
 
 export default Wyrm;
