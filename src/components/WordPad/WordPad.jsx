5f7d42733b221a8427fb1dd63a4ed9e6f235506d
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-07-10 20:35:44 -0400

    input functionality working, however formatting not. Need to research deprication of execComman

--- src/components/WordPad/WordPad.jsx
+++ src/components/WordPad/WordPad.jsx
@@ -1,17 +1,48 @@
 import './WordPad.scss';
 import Button from '../Button/Button';
+import { useState, useEffect, useRef } from 'react';
 
 const WordPad = () => {
+  const inputRef = useRef(null);
+  const [documentContent, setDocumentContent] = useState('');
+
+  const input = inputRef.current;
+
+  const handleInput = () => {
+    console.log('Input:', input.innerHtml);
+    setDocumentContent(input.innerHtml);
+  };
+
+  const handleFormat = (formatCommand) => {
+    document.execCommand(formatCommand, false, null);
+    console.log('cliced');
+  };
+
   return (
     <div className="wordpad">
       <div className="wordpad__toolbar">
-        <Button label={'Bold'} style={'start-button'} />
-        <Button label={'Italic'} style={'start-button'} />
-        <Button label={'Underline'} style={'start-button'} />
+        <Button
+          label={'Bold'}
+          style={'start-button'}
+          onClick={handleFormat('bold')}
+        />
+        <Button
+          label={'Italic'}
+          style={'start-button'}
+          onClick={handleFormat('italic')}
+        />
+        <Button
+          label={'Underline'}
+          style={'start-button'}
+          onClick={handleFormat('underline')}
+        />
       </div>
-      <div className="wordpad__input" contentEditable="true">
-        Type here
-      </div>
+      <div
+        ref={input}
+        className="wordpad__input"
+        contentEditable="true"
+        onChange={handleInput}
+      ></div>
     </div>
   );
 };
