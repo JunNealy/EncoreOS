1ecff7107630279c2548e3b94108b05b8499fd3e
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-07-14 15:53:10 -0400

    fixed hygiene typos, implemented meter display matching the need value stored in state

--- src/components/PetApp/PetApp.jsx
+++ src/components/PetApp/PetApp.jsx
@@ -9,56 +9,67 @@ import './PetApp.scss';
 const PetApp = () => {
   const [hunger, setHunger] = useState(100);
   const [happiness, setHappiness] = useState(100);
-  const [hygeine, setHygeine] = useState(100);
+  const [hygiene, setHygiene] = useState(100);
 
   const degradeNeeds = () => {
-    let diff = Math.floor(Math.random() * 4 + 1);
-    console.log(diff);
-    setHunger((prevHunger) => Math.max(prevHunger - diff, 0));
-    setHappiness((prevHappiness) => Math.max(prevHappiness - diff, 0));
-    setHygeine((prevHygeiene) => Math.max(prevHygeiene - diff, 0));
+    setHunger((prevHunger) =>
+      Math.max(prevHunger - Math.floor(Math.random() * 10 + 1), 0)
+    );
+    setHappiness((prevHappiness) =>
+      Math.max(prevHappiness - Math.floor(Math.random() * 4 + 1), 0)
+    );
+    setHygiene((prevHygiene) =>
+      Math.max(prevHygiene - Math.floor(Math.random() * 4 + 1), 0)
+    );
     console.log(hunger);
     console.log(happiness);
-    console.log(hygeine);
+    console.log(hygiene);
   };
 
   useEffect(() => {
     const needLoop = () => {
-      const interval = Math.floor(Math.random() * 10 + 1) * 1000;
+      const interval = Math.floor(Math.random() * 100 + 1) * 1000;
       setTimeout(degradeNeeds, interval);
-      setInterval(degradeNeeds, interval);
     };
 
     needLoop();
   }, []);
 
   const handleFeed = () => {
-    setHunger((prevHunger) => Math.max(prevHunger + 25, 100));
+    setHunger((prevHunger) => Math.min(prevHunger + 25, 100));
     console.log('INCREMENT HUNGER', hunger);
   };
 
   const handlePlay = () => {
-    setHunger((prevHappiness) => Math.max(prevHappiness + 25, 100));
+    setHappiness((prevHappiness) => Math.min(prevHappiness + 25, 100));
     console.log('INCREMENT HAPPINESS', happiness);
   };
 
   const handleBath = () => {
-    setHunger((prevHygeine) => Math.max(prevHygeine + 25, 100));
-    console.log('INCREMENT HYGIENE', hygeine);
+    setHygiene((prevHygiene) => Math.min(prevHygiene + 25, 100));
+    console.log('INCREMENT HYGIENE', hygiene);
   };
 
   return (
     <div className="pet-app">
       <div className="pet-app__needs">
         <div className="pet-app__needs-hunger">
-          <div className="pet-app__needs-hunger-meter"></div>
+          <div
+            className="pet-app__needs-hunger-meter"
+            style={{ width: `${hunger}%` }}
+          ></div>
         </div>
         <div className="pet-app__needs-happiness">
-          {' '}
-          <div className="pet-app__needs-happiness-meter"></div>
+          <div
+            className="pet-app__needs-happiness-meter"
+            style={{ width: `${happiness}%` }}
+          ></div>
         </div>
-        <div className="pet-app__needs-hygeine">
-          <div className="pet-app__needs-hygeine-meter"></div>
+        <div className="pet-app__needs-hygiene">
+          <div
+            className="pet-app__needs-hygiene-meter"
+            style={{ width: `${hygiene}%` }}
+          ></div>
         </div>
       </div>
       <div className="pet-app__pet-display">
@@ -67,7 +78,7 @@ const PetApp = () => {
           src={
             hunger < 50
               ? grungusHungry
-              : grungusIdle || hygeine < 50
+              : hygiene < 50
               ? grungusStinky
               : grungusIdle
           }
